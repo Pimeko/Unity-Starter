0424860de298fcb7c5efe462d0161d16ddc8863b
Pimeko <benjamin.widaw@gmail.com>
2019-03-29 03:52:20 +0100

    Add custom editor window to generate payloaded game events

---
+++ Assets/Editor/GameEvent/PayloadedGameEventGenerator.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+public class PayloadedGameEventGenerator : EditorWindow
+{
+    string type;
+    string pascalType
+    {
+        get
+        {
+            return char.ToUpper(type[0]) + type.Substring(1);
+        }
+    }
+
+    [MenuItem("Custom/Event/Payloaded/Generate")]
+    public static void ShowWindow()
+    {
+        EditorWindow.GetWindow(typeof(PayloadedGameEventGenerator));
+    }
+
+    void OnGUI()
+    {
+        GUILayout.Label("Type", EditorStyles.boldLabel);
+        type = EditorGUILayout.TextField(
+            "Type (int, PlayerData..)",
+            type,
+            GUILayout.Width(300));
+
+        if (GUILayout.Button("Generate", GUILayout.Width(150)))
+        {
+            if (type == "")
+                return;
+            
+            string basicPath = Application.dataPath + "/Scripts/General/ScriptableObjects/GameEvent/Payloaded/" + pascalType + "/";
+            string gameEventPath = basicPath + "GameEvent" + pascalType + ".cs";
+            string gameEventListenerPath = basicPath + "GameEvent" + pascalType + "Listener.cs";
+            string unityEventPath = Application.dataPath + "/Scripts/General/UnityEvents/UnityEvent" + pascalType + ".cs";
+
+            GenerateFile(gameEventPath, Application.dataPath + "/Editor/GameEvent/GameEventTemplate.txt");
+            GenerateFile(gameEventListenerPath, Application.dataPath + "/Editor/GameEvent/GameEventListenerTemplate.txt");
+            GenerateFile(unityEventPath, Application.dataPath + "/Editor/GameEvent/UnityEventTemplate.txt");
+            
+            AssetDatabase.Refresh();
+        }
+    }
+
+    void GenerateFile(string filePath, string templatePath)
+    {
+        FileInfo file = new FileInfo(filePath);
+        string templateAsString = File.ReadAllText(templatePath);
+        string content = templateAsString.Replace("[PASCAL]", pascalType);
+        content = content.Replace("[TYPE]", type);
+        file.Directory.Create();
+        File.WriteAllText(filePath, content);
+    }
+}
