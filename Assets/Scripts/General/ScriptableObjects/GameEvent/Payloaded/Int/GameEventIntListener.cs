21467a1d670c9f5e9e7c0813879ea0b1844b026c
Pimeko <benjamin.widaw@gmail.com>
2019-04-16 17:59:14 +0100

    Remove useless imports + update 'A update'

--- Assets/Scripts/General/ScriptableObjects/GameEvent/Payloaded/Int/GameEventIntListener.cs
+++ Assets/Scripts/General/ScriptableObjects/GameEvent/Payloaded/Int/GameEventIntListener.cs
@@ -1,6 +1,1 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.Events;
-
 public class GameEventIntListener : PayloadedGameEventListener<int, GameEventInt, UnityEventInt> {}
--- Assets/Scripts/General/ScriptableObjects/GameEvent/Payloaded/Int/GameEventIntRaiser.cs
+++ Assets/Scripts/General/ScriptableObjects/GameEvent/Payloaded/Int/GameEventIntRaiser.cs
@@ -1,5 +1,1 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
 public class GameEventIntRaiser : PayloadedGameEventRaiser<int, GameEventInt> {}
