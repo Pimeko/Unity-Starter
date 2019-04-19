8f12fcb508477a9008d18f43292e9b307e4ff12c
Pimeko <benjamin.widaw@gmail.com>
2019-04-19 21:08:39 +0100

    Read only on previous value for registerable scriptable objects

--- Assets/Scripts/General/ScriptableObjects/Variables/_Definitions/SimpleRegisterableScriptableObject.cs
+++ Assets/Scripts/General/ScriptableObjects/Variables/_Definitions/SimpleRegisterableScriptableObject.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using NaughtyAttributes;
 
 public class SimpleRegisterableScriptableObject<T> : RegisterableScriptableObject
 {
@@ -8,7 +9,10 @@ public class SimpleRegisterableScriptableObject<T> : RegisterableScriptableObjec
     protected T initialValue;
 
     [SerializeField]
-    protected T value, previousValue;
+    protected T value;
+    [SerializeField]
+    [ReadOnly]
+    protected T previousValue;
     public T Value { get { return value; } set { previousValue = this.value; this.value = value; TriggerChange(); } }
 	public T PreviousValue { get { return previousValue; } }
 	
