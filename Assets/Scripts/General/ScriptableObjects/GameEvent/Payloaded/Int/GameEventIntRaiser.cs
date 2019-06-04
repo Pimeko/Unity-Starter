818398643230e646055649ded6439c54bf5431f5
Benjamin Widawski <benjamin.widawski@ubisoft.com>
2019-06-04 19:36:25 +0200

    Remove all raisers related code

--- Assets/Scripts/General/ScriptableObjects/GameEvent/Payloaded/Int/GameEventIntRaiser.cs
+++
@@ -1,1 +0,0 @@
-public class GameEventIntRaiser : PayloadedGameEventRaiser<int, GameEventInt> {}
--- Assets/Scripts/General/ScriptableObjects/GameEvent/_Definitions/GameEventRaiser.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class GameEventRaiser<T_GAME_EVENT> : MonoBehaviour
-{
-    [SerializeField]
-    protected T_GAME_EVENT gameEvent;
-}
