9c826e1602bf98cc82ac501da9f26c27863e4d5a
Benjamin Widawski <benjamin.widawski@ubisoft.com>
2019-06-04 19:38:34 +0200

    Add moveTo method

---
+++ Assets/Scripts/General/Utils/Transform/TransformUtils.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransformUtils
+{
+    public static IEnumerator MoveTo(Vector3 from, Vector3 to, float duration,
+        Action<Vector3> onChange = null, Action onOver = null)
+    {
+        float t = 0;
+        Vector3 currentPosition = from;
+        
+        while (t < duration)
+        {
+            currentPosition = Vector3.Lerp(from, to, (t / duration));
+            if (onChange != null)
+                onChange(currentPosition);
+            t += Time.deltaTime;
+            yield return null;
+        }
+        if (onChange != null)
+            onChange(to);
+
+        if (onOver != null)
+            onOver();
+    }
+}
