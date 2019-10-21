d8f0a1f5b9b7ce7e6e74fd597e636c9ace7271af
Benjamin Widawski <benjamin.widawski@ubisoft.com>
2019-10-21 16:07:16 +0200

    Add smooth audio source

---
+++ Assets/Scripts/Utils/AudioSource/SmoothAudioSource.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class SmoothAudioSource : MonoBehaviour
+{
+    AudioSource audioSource;
+    AudioSource CurrentAudioSource => transform.CachedComponent(ref audioSource);
+
+    bool isPlaying;
+    float volumeToReach;
+    Tween currentTween;
+
+    private void Start()
+    {
+        volumeToReach = CurrentAudioSource.volume;
+        currentTween = null;
+    }
+
+    public void Play()
+    {
+        if (!isPlaying)
+        {
+            DOTweenUtils.KillTween(ref currentTween);
+            currentTween = DOVirtual.Float(0, volumeToReach, .2f, newVolume => CurrentAudioSource.volume = newVolume);
+            CurrentAudioSource.Play();
+            isPlaying = true;
+        }
+    }
+
+    public void Stop()
+    {
+        if (isPlaying)
+        {
+            DOTweenUtils.KillTween(ref currentTween);
+            currentTween = DOVirtual.Float(volumeToReach, 0, .2f, newVolume => CurrentAudioSource.volume = newVolume).OnComplete(CurrentAudioSource.Play);
+            isPlaying = false;
+        }
+    }
+}
