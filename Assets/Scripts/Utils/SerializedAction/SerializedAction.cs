0bae6d0d4f29183ea4d46e105e56f252aa5e7a79
Benjamin Widawski <benjamin.widawski@ubisoft.com>
2019-11-12 17:48:58 +0100

    Add SerializedAction

---
+++ Assets/Scripts/Utils/SerializedAction/SerializedAction.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SerializedAction : MonoBehaviour
+{
+    [SerializeField]
+    DelayedUnityEvent action;
+    
+    public void Do()
+    {
+        action.Invoke();
+    }
+}
