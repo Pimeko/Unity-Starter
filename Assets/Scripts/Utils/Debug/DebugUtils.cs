0e7da25146b1ebc854ac1bb0626ced72e3751875
Benjamin Widawski <benjamin.widawski@ubisoft.com>
2019-12-11 16:01:16 +0100

    Add duration to draw point

--- Assets/Scripts/Utils/Debug/DebugUtils.cs
+++ Assets/Scripts/Utils/Debug/DebugUtils.cs
@@ -5,11 +5,11 @@ using UnityEngine;
 
 public class DebugUtils
 {
-    public static void DrawPoint(Vector3 position)
+    public static void DrawPoint(Vector3 position, Color color, float duration)
     {
-        Debug.DrawLine(position - new Vector3(1, 0, 0), position + new Vector3(1, 0, 0), Color.white);
-        Debug.DrawLine(position - new Vector3(0, 0, 1), position + new Vector3(0, 0, 1), Color.white);
-        Debug.DrawLine(position - new Vector3(0, 1, 0), position + new Vector3(0, 1, 0), Color.white);
+        Debug.DrawLine(position - new Vector3(1, 0, 0), position + new Vector3(1, 0, 0), color, duration);
+        Debug.DrawLine(position - new Vector3(0, 0, 1), position + new Vector3(0, 0, 1), color, duration);
+        Debug.DrawLine(position - new Vector3(0, 1, 0), position + new Vector3(0, 1, 0), color, duration);
     }
 
     public static void PrintNull(object o)
