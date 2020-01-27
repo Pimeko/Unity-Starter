b12581a3688e0ee734ade5d925510ca661442bb3
Benjamin Widawski <benjamin.widaw@gmail.com>
2020-01-27 19:14:42 +0100

    Better animation receiver

---
+++ Assets/Scripts/Utils/Animation/AnimationReceiver.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class AnimationReceiver : MonoBehaviour
+{
+    public Action<string> OnAction;
+
+    public void Invoke(string a)
+    {
+        OnAction?.Invoke(a);
+    }
+}
