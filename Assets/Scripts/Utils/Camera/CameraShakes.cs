93f53329e6b21ba28592d927cebf94603111dabd
Benjamin Widawski <benjamin.widaw@gmail.com>
2020-01-29 00:07:24 +0100

    Better camera shakes handling of parallell tweens

--- Assets/Scripts/Utils/Camera/CameraShakes.cs
+++ Assets/Scripts/Utils/Camera/CameraShakes.cs
@@ -10,152 +10,249 @@ public class CameraShakes : MonoBehaviour
 {
     Camera cam;
 
+    Vector3 currentInitialPosition;
+    float currentFov;
+    Sequence currentSequencePosition, currentSequenceFov;
+
     private void Start()
     {
         cam = GetComponent<Camera>();
+
+        currentSequencePosition = null;
+        currentSequenceFov = null;
+    }
+
+    void InitPosition()
+    {
+        if (currentSequencePosition == null)
+            currentInitialPosition = transform.localPosition;
+    }
+
+    void InitFov()
+    {
+        if (currentSequenceFov == null)
+            currentFov = cam.fieldOfView;
     }
 
     [Button, TabGroup("Vertical")]
     public void SmallShakeVertical(Action callback)
     {
-        Vector3 initialPosition = transform.localPosition;
-        DOTween.Sequence()
+        InitPosition();
+        
+        currentSequencePosition = DOTween.Sequence()
            .Append(transform.DOLocalMove(transform.localPosition + transform.up * 0.05f, .1f))
            .Append(transform.DOLocalMove(transform.localPosition - transform.up * 0.05f, .1f))
            .Append(transform.DOLocalMove(transform.localPosition, .1f))
-           .OnComplete(() => { transform.localPosition = initialPosition; callback?.Invoke(); });
+           .OnComplete(() =>
+           {
+               transform.localPosition = currentInitialPosition;
+               currentSequencePosition = null;
+               callback?.Invoke();
+           });
     }
 
     [Button, TabGroup("Vertical")]
     public void ShakeVertical(Action callback)
     {
-        Vector3 initialPosition = transform.localPosition;
-        DOTween.Sequence()
+        InitPosition();
+        
+        currentSequencePosition = DOTween.Sequence()
            .Append(transform.DOLocalMove(transform.localPosition + transform.up * 0.2f, .1f))
            .Append(transform.DOLocalMove(transform.localPosition - transform.up * 0.2f, .1f))
            .Append(transform.DOLocalMove(transform.localPosition, .1f))
-           .OnComplete(() => { transform.localPosition = initialPosition; callback?.Invoke(); });
+           .OnComplete(() =>
+           {
+               transform.localPosition = currentInitialPosition;
+               currentSequencePosition = null;
+               callback?.Invoke();
+           });
     }
 
     [Button, TabGroup("Vertical")]
     public void LongShakeVertical(Action callback)
     {
-        Vector3 initialPosition = transform.localPosition;
-        DOTween.Sequence()
+        InitPosition();
+        
+        currentSequencePosition = DOTween.Sequence()
            .Append(transform.DOLocalMove(transform.localPosition + transform.up * 0.2f, .1f))
            .Append(transform.DOLocalMove(transform.localPosition - transform.up * 0.2f, .1f))
            .Append(transform.DOLocalMove(transform.localPosition + transform.up * 0.2f, .1f))
            .Append(transform.DOLocalMove(transform.localPosition - transform.up * 0.2f, .1f))
            .Append(transform.DOLocalMove(transform.localPosition, .1f))
-           .OnComplete(() => { transform.localPosition = initialPosition; callback?.Invoke(); });
+           .OnComplete(() =>
+           {
+               transform.localPosition = currentInitialPosition;
+               currentSequencePosition = null;
+               callback?.Invoke();
+           });
     }
 
     [Button, TabGroup("Horizontal")]
     public void VerySmallShakeHorizontal(Action callback)
     {
-        Vector3 initialPosition = transform.localPosition;
-        DOTween.Sequence()
+        InitPosition();
+        
+        currentSequencePosition = DOTween.Sequence()
            .Append(transform.DOLocalMove(transform.localPosition + transform.right * .02f, .1f))
            .Append(transform.DOLocalMove(transform.localPosition - transform.right * .02f, .1f))
            .Append(transform.DOLocalMove(transform.localPosition, .1f))
-           .OnComplete(() => { transform.localPosition = initialPosition; callback?.Invoke(); });
+           .OnComplete(() =>
+           {
+               transform.localPosition = currentInitialPosition;
+               currentSequencePosition = null;
+               callback?.Invoke();
+           });
     }
 
     [Button, TabGroup("Horizontal")]
     public void SmallShakeHorizontal(Action callback)
     {
-        Vector3 initialPosition = transform.localPosition;
-        DOTween.Sequence()
+        InitPosition();
+        
+        currentSequencePosition = DOTween.Sequence()
            .Append(transform.DOLocalMove(transform.localPosition + transform.right * 0.05f, .1f))
            .Append(transform.DOLocalMove(transform.localPosition - transform.right * 0.05f, .1f))
            .Append(transform.DOLocalMove(transform.localPosition, .1f))
-           .OnComplete(() => { transform.localPosition = initialPosition; callback?.Invoke(); });
+           .OnComplete(() =>
+           {
+               transform.localPosition = currentInitialPosition;
+               currentSequencePosition = null;
+               callback?.Invoke();
+           });
     }
 
     [Button, TabGroup("Horizontal")]
     public void MediumShakeHorizontal(Action callback)
     {
-        Vector3 initialPosition = transform.localPosition;
-        DOTween.Sequence()
+        InitPosition();
+        
+        currentSequencePosition = DOTween.Sequence()
            .Append(transform.DOLocalMove(transform.localPosition + transform.right * 0.15f, .1f))
            .Append(transform.DOLocalMove(transform.localPosition - transform.right * 0.15f, .1f))
            .Append(transform.DOLocalMove(transform.localPosition, .1f))
-           .OnComplete(() => { transform.localPosition = initialPosition; callback?.Invoke(); })
+           .OnComplete(() =>
+           {
+               transform.localPosition = currentInitialPosition;
+               currentSequencePosition = null;
+               callback?.Invoke();
+           })
             .SetUpdate(true);
     }
 
     [Button, TabGroup("Horizontal")]
     public void ShakeHorizontal(Action callback)
     {
-        Vector3 initialPosition = transform.localPosition;
-        DOTween.Sequence()
+        InitPosition();
+        
+        currentSequencePosition = DOTween.Sequence()
            .Append(transform.DOLocalMove(transform.localPosition + transform.right * 0.2f, .1f))
            .Append(transform.DOLocalMove(transform.localPosition - transform.right * 0.2f, .1f))
            .Append(transform.DOLocalMove(transform.localPosition, .1f))
-           .OnComplete(() => { transform.localPosition = initialPosition; callback?.Invoke(); })
+           .OnComplete(() =>
+           {
+               transform.localPosition = currentInitialPosition;
+               currentSequencePosition = null;
+               callback?.Invoke();
+           })
             .SetUpdate(true);
     }
 
     [Button, TabGroup("Horizontal")]
     public void LongShakeHorizontal(Action callback)
     {
-        Vector3 initialPosition = transform.localPosition;
-        DOTween.Sequence()
+        InitPosition();
+        
+        currentSequencePosition = DOTween.Sequence()
            .Append(transform.DOLocalMove(transform.localPosition + transform.right * 0.2f, .1f))
            .Append(transform.DOLocalMove(transform.localPosition - transform.right * 0.2f, .1f))
            .Append(transform.DOLocalMove(transform.localPosition + transform.right * 0.2f, .1f))
            .Append(transform.DOLocalMove(transform.localPosition - transform.right * 0.2f, .1f))
            .Append(transform.DOLocalMove(transform.localPosition, .1f))
-           .OnComplete(() => { transform.localPosition = initialPosition; callback?.Invoke(); });
+           .OnComplete(() =>
+           {
+               transform.localPosition = currentInitialPosition;
+               currentSequencePosition = null;
+               callback?.Invoke();
+           });
     }
 
     [Button, TabGroup("Zoom")]
     public void SmallZoomOutZoomIn(Action callback)
     {
-        Vector3 initialPosition = transform.localPosition;
-        DOTween.Sequence()
+        InitPosition();
+        
+        currentSequencePosition = DOTween.Sequence()
            .Append(transform.DOLocalMove(transform.localPosition - transform.forward / 2, .15f))
            .Append(transform.DOLocalMove(transform.localPosition, .15f))
-           .OnComplete(() => { transform.localPosition = initialPosition; callback?.Invoke(); });
+           .OnComplete(() =>
+           {
+               transform.localPosition = currentInitialPosition;
+               currentSequencePosition = null;
+               callback?.Invoke();
+           });
     }
 
     [Button, TabGroup("Zoom")]
     public void ZoomOutZoomInElastic(Action callback)
     {
-        Vector3 initialPosition = transform.localPosition;
-        DOTween.Sequence()
+        InitPosition();
+        
+        currentSequencePosition = DOTween.Sequence()
            .Append(transform.DOLocalMove(transform.localPosition - transform.forward, .5f))
            .Append(transform.DOLocalMove(transform.localPosition, .5f).SetEase(Ease.OutElastic))
-           .OnComplete(() => { transform.localPosition = initialPosition; callback?.Invoke(); });
+           .OnComplete(() =>
+           {
+               transform.localPosition = currentInitialPosition;
+               currentSequencePosition = null;
+               callback?.Invoke();
+           });
     }
 
     [Button, TabGroup("Zoom")]
     public void SmallZoomOutZoomInFov(Action callback)
     {
-        float initialFov = cam.fieldOfView;
-        DOTween.Sequence()
-           .Append(DOVirtual.Float(initialFov, initialFov + 2, .15f, newFov => cam.fieldOfView = newFov))
-           .Append(DOVirtual.Float(initialFov + 2, initialFov, .15f, newFov => cam.fieldOfView = newFov))
-           .OnComplete(() => { cam.fieldOfView = initialFov; callback?.Invoke(); });
+        InitFov();
+        
+        currentSequenceFov = DOTween.Sequence()
+           .Append(DOVirtual.Float(currentFov, currentFov + 2, .15f, newFov => cam.fieldOfView = newFov))
+           .Append(DOVirtual.Float(currentFov + 2, currentFov, .15f, newFov => cam.fieldOfView = newFov))
+           .OnComplete(() =>
+           {
+               cam.fieldOfView = currentFov;
+               currentSequenceFov = null;
+               callback?.Invoke();
+           });
     }
 
     [Button, TabGroup("Zoom")]
     public void ZoomInZoomOutFov(Action callback)
     {
-        float initialFov = cam.fieldOfView;
-        DOTween.Sequence()
-           .Append(DOVirtual.Float(initialFov, initialFov - 5, 1, newFov => cam.fieldOfView = newFov))
-           .Append(DOVirtual.Float(initialFov - 5, initialFov, .5f, newFov => cam.fieldOfView = newFov))
-           .OnComplete(() => { cam.fieldOfView = initialFov; callback?.Invoke(); });
+        InitFov();
+        
+        currentSequenceFov = DOTween.Sequence()
+           .Append(DOVirtual.Float(currentFov, currentFov - 5, 1, newFov => cam.fieldOfView = newFov))
+           .Append(DOVirtual.Float(currentFov - 5, currentFov, .5f, newFov => cam.fieldOfView = newFov))
+           .OnComplete(() =>
+           {
+               cam.fieldOfView = currentFov;
+               currentSequenceFov = null;
+               callback?.Invoke();
+           });
     }
 
     [Button, TabGroup("Zoom")]
     public void ZoomOutZoomInElasticFov(Action callback)
     {
-        float initialFov = cam.fieldOfView;
-        DOTween.Sequence()
-           .Append(DOVirtual.Float(initialFov, initialFov + 5, .5f, newFov => cam.fieldOfView = newFov))
-           .Append(DOVirtual.Float(initialFov + 5, initialFov, .5f, newFov => cam.fieldOfView = newFov).SetEase(Ease.OutElastic))
-           .OnComplete(() => { cam.fieldOfView = initialFov; callback?.Invoke(); });
+        InitFov();
+        
+        currentSequenceFov = DOTween.Sequence()
+           .Append(DOVirtual.Float(currentFov, currentFov + 5, .5f, newFov => cam.fieldOfView = newFov))
+           .Append(DOVirtual.Float(currentFov + 5, currentFov, .5f, newFov => cam.fieldOfView = newFov).SetEase(Ease.OutElastic))
+           .OnComplete(() =>
+           {
+               cam.fieldOfView = currentFov;
+               currentSequenceFov = null;
+               callback?.Invoke();
+           });
     }
 }
