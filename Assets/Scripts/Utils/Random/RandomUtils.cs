0af2a757d092b46aa387be869a56f4d097ecce42
Benjamin Widawski <benjamin.widaw@gmail.com>
2020-04-15 21:03:54 +0200

    Add RandomPositionOnQuad

--- Assets/Scripts/Utils/Random/RandomUtils.cs
+++ Assets/Scripts/Utils/Random/RandomUtils.cs
@@ -8,4 +8,16 @@ public static class RandomUtils
     {
         return Random.Range(0f, 1f) < .5f;
     }
+
+    public static Vector3 RandomPositionOnQuad(Transform quad)
+    {
+        var halfWidth = quad.lossyScale.x / 2;
+        var halfHeight = quad.lossyScale.y / 2;
+
+        return new Vector3(
+            quad.position.x + Random.Range(-halfWidth, halfWidth),
+            quad.position.y,
+            quad.position.z + Random.Range(-halfHeight, halfHeight)
+        );
+    }
 }
