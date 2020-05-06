920ef7dc7dc7fa3f1a3c4e94897d919c2965f0fa
Benjamin Widawski <benjamin.widaw@gmail.com>
2020-05-06 20:36:37 +0200

    Add OnDestroy callback

---
+++ Assets/Scripts/Utils/NativeCallbacks/OnDestroyCallback.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class OnDestroyCallback : NativeCallback
+{
+    void OnDestroy()
+    {
+        DoActions();
+    }
+}
