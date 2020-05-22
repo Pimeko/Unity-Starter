5b4fa0a6664f58772b742efbc1e3024569f79e37
Benjamin Widawski <benjamin.widaw@gmail.com>
2020-05-23 01:45:48 +0200

    Disable draggable until fix

--- Assets/Editor/GameEvent/DraggableBasicGameEventEditor.cs
+++ Assets/Editor/GameEvent/DraggableBasicGameEventEditor.cs
@@ -6,38 +6,38 @@ using System.Collections.Generic;
 [InitializeOnLoad]
 public class DraggableBasicGameEventEditor : Editor
 {
-    static DraggableBasicGameEventEditor()
-    {
-        // Adds a callback for when the hierarchy window processes GUI events
-        // for every GameObject in the heirarchy.
-        EditorApplication.hierarchyWindowItemOnGUI += HierarchyWindowItemCallback;
-    }
+    // static DraggableBasicGameEventEditor()
+    // {
+    //     // Adds a callback for when the hierarchy window processes GUI events
+    //     // for every GameObject in the heirarchy.
+    //     EditorApplication.hierarchyWindowItemOnGUI += HierarchyWindowItemCallback;
+    // }
 
-    static void HierarchyWindowItemCallback(int instanceID, Rect pRect)
-    {
-        // happens when an acceptable item is released over the GUI window
-        if (Event.current.type == EventType.DragPerform && pRect.Contains(Event.current.mousePosition))
-        {
-            // get all the drag and drop information ready for processing.
-            DragAndDrop.AcceptDrag();
+    // static void HierarchyWindowItemCallback(int instanceID, Rect pRect)
+    // {
+    //     // happens when an acceptable item is released over the GUI window
+    //     if (Event.current.type == EventType.DragPerform && pRect.Contains(Event.current.mousePosition))
+    //     {
+    //         // get all the drag and drop information ready for processing.
+    //         DragAndDrop.AcceptDrag();
 
-            GameObject gameObject = EditorUtility.InstanceIDToObject(instanceID) as GameObject;
+    //         GameObject gameObject = EditorUtility.InstanceIDToObject(instanceID) as GameObject;
 
-            if (gameObject == null)
-                return;
+    //         if (gameObject == null)
+    //             return;
 
-            // run through each object that was dragged in.
-            foreach (var objectRef in DragAndDrop.objectReferences)
-            {
-                // if the object is the particular asset type...
-                if (objectRef is BasicGameEvent)
-                {
-                    var listener = gameObject.AddComponent<BasicGameEventListener>();
-                    listener.AddGameEvent((BasicGameEvent)objectRef);
-                }
-            }
+    //         // run through each object that was dragged in.
+    //         foreach (var objectRef in DragAndDrop.objectReferences)
+    //         {
+    //             // if the object is the particular asset type...
+    //             if (objectRef is BasicGameEvent)
+    //             {
+    //                 var listener = gameObject.AddComponent<BasicGameEventListener>();
+    //                 listener.AddGameEvent((BasicGameEvent)objectRef);
+    //             }
+    //         }
 
-            Event.current.Use();
-        }
-    }
+    //         Event.current.Use();
+    //     }
+    // }
 }
