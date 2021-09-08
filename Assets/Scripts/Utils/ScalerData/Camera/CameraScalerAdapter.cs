efb7ec9350c1ab51e74ec984d75b8b4bbe264768
Benjamin Widawski <benjamin.widaw@gmail.com>
2021-09-08 13:47:47 +0200

    More options for Camera Scaler Adapter

--- Assets/Scripts/Utils/ScalerData/Camera/CameraScalerAdapter.cs
+++ Assets/Scripts/Utils/ScalerData/Camera/CameraScalerAdapter.cs
@@ -9,6 +9,10 @@ public class CameraScalerAdapter : MonoBehaviour
 {
     [SerializeField]
     CameraScalerDataVariable data;
+    [SerializeField]
+    float transitionDuration = .15f;
+    [SerializeField, InfoBox("Follow offset or Local Position")]
+    bool useLocalPosition = false;
 
     CinemachineVirtualCamera virtualCamera;
     CinemachineTransposer cinemachineTransposer;
@@ -30,16 +34,21 @@ public class CameraScalerAdapter : MonoBehaviour
             DOVirtual.Float(
                 virtualCamera.m_Lens.FieldOfView,
                 currentData.fov,
-                .15f,
+                transitionDuration,
                 newValue => virtualCamera.m_Lens.FieldOfView = newValue).SetEase(Ease.InCirc);
         }
         if (currentData.usePosition)
         {
-            DOTween.To(
-                () => cinemachineTransposer.m_FollowOffset,
-                offset => cinemachineTransposer.m_FollowOffset = offset,
-                currentData.offsetPosition,
-                .15f).SetEase(Ease.InCirc);
+            if (useLocalPosition)
+                transform.DOLocalMove(currentData.offsetPosition, transitionDuration).SetEase(Ease.InCirc);
+            else
+            {
+                DOTween.To(
+                    () => cinemachineTransposer.m_FollowOffset,
+                    offset => cinemachineTransposer.m_FollowOffset = offset,
+                    currentData.offsetPosition,
+                    transitionDuration).SetEase(Ease.InCirc);
+            }
         }
     }
 }
