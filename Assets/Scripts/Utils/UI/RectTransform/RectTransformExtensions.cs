96693cfd02e28be5d65005e5904978688b3589f6
Benjamin Widawski <benjamin.widaw@gmail.com>
2022-02-21 16:16:30 +0100

    Add SetTop etc for Rect Transform

--- Assets/Scripts/Utils/UI/RectTransform/RectTransformExtensions.cs
+++ Assets/Scripts/Utils/UI/RectTransform/RectTransformExtensions.cs
@@ -10,4 +10,24 @@ public static class RectTransformExtensions
         Vector2 size = Vector2.Scale(transform.rect.size, transform.lossyScale);
         return new Rect((Vector2)transform.position - (size * 0.5f), size);
     }
+
+    public static void SetLeft(this RectTransform rt, float left)
+    {
+        rt.offsetMin = new Vector2(left, rt.offsetMin.y);
+    }
+
+    public static void SetRight(this RectTransform rt, float right)
+    {
+        rt.offsetMax = new Vector2(-right, rt.offsetMax.y);
+    }
+
+    public static void SetTop(this RectTransform rt, float top)
+    {
+        rt.offsetMax = new Vector2(rt.offsetMax.x, -top);
+    }
+
+    public static void SetBottom(this RectTransform rt, float bottom)
+    {
+        rt.offsetMin = new Vector2(rt.offsetMin.x, bottom);
+    }
 }
